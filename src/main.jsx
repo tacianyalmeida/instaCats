a8bf3f75ed3fe503a067bb6a61509cafec27fa3f
Taciany Maria S <138811627+tacianyalmeida@users.noreply.github.com>
2024-03-25 18:19:29 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import './main'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Header/>
+    <Form/>
+    <Footer/>
+  
+  </React.StrictMode>,
+)
